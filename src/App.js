ef123f7663ef1bc933e89f31459c10538c3bd8e6
b1swanath <biswanathtudu100@gmail.com>
2024-12-19 15:06:22 +0530

    new2

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <div>
       <Router>
         <Routes>
-          <Route path='/' element={<Login />} />
+          <Route path='/Z-React' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='*' element={<Notfound />} />
